20c46a121722887c3486dbe8f66f7fc289643947
Prisha Maiti <prisha.maiti@gmail.com>
2022-04-24 18:37:18 -0700

    Completed rowSums Method of FRQ 1

--- FRQs/DiverseArray.java
+++ FRQs/DiverseArray.java
@@ -10,15 +10,17 @@ public class DiverseArray {
         return sum;
     }
     public static int[] rowSums(int[][] arr2D) {
-        int rows;
-        ArrayList<Integer> arr = new ArrayList<Integer>;
-        for(int r = 0; r < arr.length(); r++) {
-            arr.add(arraySum(arr));
+        int arrResult[] = new int[arr2D.length];
+        for(int i = 0; i < arr2D.length; i++){
+            int newTotal = 0;
+            for(int j = 0; j < arr2D[i].length; j++) {
+                  newTotal += arr2D [i][j];
+            }
+            arrResult[i] = newTotal;
         }
-        int[] arrResult = arr;
         return arrResult;
     }
-    public static boolean isDiverse(int[][] arr2D) {
+    /* public static boolean isDiverse(int[][] arr2D) {
         boolean diversity = true;
         for(int i = 0; i < arr2D.rows.length; i++) {
             for(int j = i + 1; j < arr2D.rows.length(); j++) {
@@ -28,7 +30,7 @@ public class DiverseArray {
             }
         }
         return diversity;
-    }
+    } */
     public static void main(String[] args) {
         int[] arr = {1, 3, 2, 7, 3};
         arraySum(arr);
