c4d3645d8f347509796ffc3fdb69ce1cd7a93673
PrishaMaiti <prisha.maiti@gmail.com>
2022-05-02 20:46:11 -0700

    Added 2020 FRQ 3

--- FRQs/ReviewCollector.java
+++ FRQs/ReviewCollector.java
@@ -1,23 +1,25 @@
 import java.util.ArrayList;
 
 public class ReviewCollector {
-    private ArrayList<ProductReview> reviewList;
-    private ArrayList<String> productList;
+    private ArrayList<ProductReview> productReviewList;
 
     public ReviewCollector() {
-        reviewList = new ArrayList<ProductReview>();
-        productList = new ArrayList<String>();
+        productReviewList = new ArrayList<ProductReview>();
+    }
+
+    public ReviewCollector(ArrayList<ProductReview> prList) {
+        productReviewList = prList;
     }
 
     public void addReview(ProductReview prodReview) {
-        reviewList.add(prodReview);
-        productList.add(prodReview.getName());
+        productReviewList.add(prodReview);
     }
 
     public int getNumGoodReviews(String prodName) {
         int numGoodReviews = 0;
-        for(int i = 0; i < productList.size(); i++) {
-            if(productList.get(i).contains("best")) {
+        for(int i = 0; i < productReviewList.size(); i++) {
+            ProductReview pr = productReviewList.get(i);
+            if(pr.getName().equals(prodName) && pr.getReview().contains("best")) {
                 numGoodReviews++;
             }
         }
--- FRQs/ReviewCollectorTester.java
+++ FRQs/ReviewCollectorTester.java
@@ -2,18 +2,32 @@ import java.util.ArrayList;
 
 public class ReviewCollectorTester {
 
-    public static ArrayList<String> buildReviewList() { // test case for part a
-        
+    public static ArrayList<ProductReview> buildProductReviewList() {
+        ArrayList<ProductReview> prList = new ArrayList<ProductReview>();
+        prList.add(new ProductReview("iPhone13", "This is the best product I've ever used!"));
+        prList.add(new ProductReview("iPhone7", "This is very good!"));
+        prList.add(new ProductReview("iPhone13", "Not the best I have used so far"));
+        prList.add(new ProductReview("iPhone13", "Best Best!"));
+        return prList;
+    }
+
+    public static void testCase1() {
+        ReviewCollector reviewCollect = new ReviewCollector(); // use ctor empty
+        reviewCollect.addReview(new ProductReview("iPhone13", "This is the best product I've ever used!"));
+        reviewCollect.addReview(new ProductReview("iPhone7", "This is very good!"));
+        reviewCollect.addReview(new ProductReview("iPhone13", "Not the best I have used so far"));
+        reviewCollect.addReview(new ProductReview("iPhone13", "Best Best best!"));
+
+        System.out.println("Good Reviewes = " + reviewCollect.getNumGoodReviews("iPhone7"));
     }
 
     public static void testCase2() { // test case for part b
-        ArrayList<String> reviewList = buildReviewList();
-        ReviewCollector reviewCollect = new ReviewCollector();
-        System.out.println(reviewCollect.getNumGoodReviews("games"));
+        ReviewCollector reviewCollect = new ReviewCollector(buildProductReviewList());
+        System.out.println("Good Reviewes = " + reviewCollect.getNumGoodReviews("iPhone13"));
     }
 
     public static void main(String[] args) {
-        //testCase1();
-        testCase2();
+        testCase1();
+        //testCase2();
     }
 }
